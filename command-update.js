f55271dc017ad2c4218f5d8f749c1a50ae239211
Lucy Farrell <lucinamfarrell@gmail.com>
2022-09-11 14:01:31 -0700

    Added config.json namechange

--- command-update.js
+++ command-update.js
@@ -1,6 +1,6 @@
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { token, app_id } = require('./secrets.json');
+const { token, app_id } = require('./config.json');
 const commands = require('./commands.json');
 
 const rest = new REST({ version: '9' }).setToken(token);
